8f7f77696f0bb897696458b3795ffaeb392663db
Lukáš Kratochvíl <kratochvil.lukas9@gmail.com>
2020-03-17 23:29:11 +0100

    removed html from server

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,14 @@
 import express from 'express';
-import * as path from 'path';
 
 const app = express();
+const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '/index.html'));
+    res.send(
+        `<p style="text-align: center;">Server is listening on port ${PORT}!</p>`
+    );
 });
 
-const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     // eslint-disable-next-line no-console
     console.log(`Server is listening on port ${PORT}!`);
